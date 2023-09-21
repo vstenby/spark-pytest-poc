0ccec166bca83ae6be210d7f8265948d014817d8
Viktor Stenby Johansson <viktor.stenby@vippsmobilepay.com>
2023-09-21 16:00:28 +0200

    Added GitHub Actions and spark test script

---
+++ test.py
@@ -0,0 +1,33 @@
+import pyspark.sql.functions as F
+
+def concatenate_columns(dataf):
+        '''
+        Add a column (l3) as the concatenation of l1 and l2, after which l1 and l2 is dropped.
+        '''
+
+        dataf = dataf.withColumn('l3', F.concat(F.col('l1'), F.col('l2')))\
+                    .drop(F.col('l1'))\
+                    .drop(F.col('l2'))
+
+        return dataf
+
+def main():
+
+    input_df = spark.createDataFrame(
+        [
+            (1, "foo", "bar"),  # Add your data here
+            (2, "bar", "bar"),
+        ],  
+        "id int, l1 string, l2 string",  # add column names and types here
+    )
+
+    expected_df = spark.createDataFrame(
+        [
+            (1, "foobar"),  # Add your data here
+            (2, "barbar"),
+        ],  
+        "id int, l3 string",  # add column names and types here
+    )
+
+    #Check that the concatenate_columns(dataf) function works as expected.
+    assert concatenate_columns(input_df).collect() == expected_df.collect(), "The function does not work as expected."
